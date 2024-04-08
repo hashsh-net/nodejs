9f912be70de69c869d8a79f5740085677eac1d6f
inunekko <tun0amail@gmail.com>
2024-04-09 03:59:28 +0900

    new

--- 1-scraping/kokonara/cstrade.js
+++ 1-scraping/kokonara/cstrade.js
@@ -1,15 +1,10 @@
 import { chromium } from 'playwright';
 
-import { page, retryPageLoad } from '../../lib/playwright.js';
-import fs from 'fs';
-
 
 const siteUrlArray = [ 
     "https://csgoempire.com/withdraw/steam/market"
 ];
 
-const from = 1;
-const to = 999;
 
 async function main() {
     const browser = await chromium.launch();
@@ -17,21 +12,21 @@ async function main() {
     const page = await context.newPage();
 
     for(const siteUrl of siteUrlArray) {
-        for (let itemNumber = from; itemNumber <= to; itemNumber++) {
-            await retryPageLoad(page, siteUrl);
 
-            // 正しいセレクターを使用して要素を取得する
-            const element = await page.$('img[src^="https://community.cloudflare.steamstatic.com/"]');
+      await page.goto(siteUrl)
 
-            // 要素が見つかった場合
-            if (element) {
-                // 要素からテキストを取得する
-                const text = await page.evaluate(element => element.getAttribute('alt'), element);
-                console.log(text); // 結果を出力
-            } else {
-                console.log('要素が見つかりませんでした');
-            }
-        }
+      // 正しいセレクターを使用して要素を取得する
+      const element = await page.$('img[src^="https://community.cloudflare.steamstatic.com/"]');
+      console.log(element)
+
+      // 要素が見つかった場合
+      if (element) {
+          // 要素からテキストを取得する
+          const text = await page.evaluate(element => element.getAttribute('alt'), element);
+          console.log(text); // 結果を出力
+      } else {
+          console.log('要素が見つかりませんでした');
+      }
     }
 
     await browser.close();

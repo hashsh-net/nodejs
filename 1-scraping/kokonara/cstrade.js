9c038e08f73a9c8a5fe06f10aab0fd855a00f864
inunekko <tun0amail@gmail.com>
2024-04-11 18:12:59 +0900

    kanse

--- 1-scraping/kokonara/cstrade.js
+++ 1-scraping/kokonara/cstrade.js
@@ -7,9 +7,10 @@ async function main() {
   const browser = await chromium.launch({ headless: false });
   const context = await browser.newContext();
   const page = await context.newPage();
-  let linetext = "てすとだよ";
+  let linetext;
   let lineUrl;
   let lineName;
+  let linePrice;
   let nyusatuelement;
 
   for (const siteUrl of siteUrlArray) {
@@ -21,6 +22,15 @@ async function main() {
     await page.waitForTimeout(1000);
     const button2 = page.locator('[for="trade_type_filter_auctions_only"]').locator("svg.h-full.w-full");
     button2.click();
+
+    await page.waitForTimeout(500);
+    //プライスの範囲を入力してアプライ
+    await page.locator("#scrollable-sidebar-element > div:nth-child(1) > div.price-range.mt-lg.flex.items-center.justify-between > div:nth-child(1) > input").fill("50");
+    await page.waitForTimeout(300);
+    const button3 = page.locator("text = Apply");
+    button3.click();
+    //プライス範囲入力終了
+
     await page.waitForTimeout(500);
     const elements = await page.$$('div[class^="relative rounded"]');
 
@@ -30,37 +40,61 @@ async function main() {
         for (const paragraph of paragraphElements) {
           const textInsideParagraph = await paragraph.textContent();
           console.log(textInsideParagraph);
-          linetext = textInsideParagraph;
-          nyusatuelement = element;
-          //console.log(nyusatuelement);
+          if (textInsideParagraph == "1" || textInsideParagraph == "2") {
+            linetext = textInsideParagraph;
+            nyusatuelement = element;
+          }
         }
       }
     } else {
       console.log('要素が見つかりませんでした');
     }
 
-    //入札のあったelementからurlを取得している
-    const nyusatuelement2 = await nyusatuelement.$$eval('a[href^="/item/"]', nodes => nodes.length > 0 ? nodes[0].href : null);
-    if (nyusatuelement2) {
-    lineUrl = nyusatuelement2;
-    }
-    //urlおわり
+    if (nyusatuelement) {
+      const nyusatuelement2 = await nyusatuelement.$$eval('a[href^="/item/"]', nodes => nodes.length > 0 ? nodes[0].href : null);
+      if (nyusatuelement2) {
+        lineUrl = nyusatuelement2;
+      }
 
-    //入札のあったelementから武器の名前を取得
-    const nyusatuelement3 = await nyusatuelement.$eval('p.size-medium', el => el ? el.textContent.trim() : null);
-    if(nyusatuelement3){
-    lineName = nyusatuelement3;
-    } else {
-    console.log('武器が取得できませんでした');
-    }
-    //武器の名前取得終わり
+      const nyusatuelement3 = await nyusatuelement.$eval('p.size-medium', el => el ? el.textContent.trim() : null);
+      if (nyusatuelement3) {
+        lineName = nyusatuelement3;
+      } else {
+        console.log('武器が取得できませんでした');
+      }
 
+      const priceElement = await nyusatuelement.$eval('span[data-v-cacaa161]', el => el ? el.textContent.trim() : null);
+      if (priceElement) {
+        const priceText = priceElement.replace(/[^0-9.]/g, '');
+        linePrice = parseFloat(priceText);
+      }
+    }
   }
 
   const token = "r273lQRIbRaiiekIylt5P2LaoMoiBMm4byFCh8krBRv";
-  await sendTextMessage(token, linetext);
-  await sendTextMessage(token, lineUrl);
-  await sendTextMessage(token, lineName);
+  if (linetext) {
+    await sendTextMessage(token, linetext);
+  } else {
+    console.log('入札数が取得できませんでした');
+  }
+
+  if (lineUrl) {
+    await sendTextMessage(token, lineUrl);
+  } else {
+    console.log('URLが取得できませんでした');
+  }
+
+  if (lineName) {
+    await sendTextMessage(token, lineName);
+  } else {
+    console.log('武器の名前が取得できませんでした');
+  }
+
+  if (linePrice !== undefined) {
+    await sendTextMessage(token, linePrice.toString());
+  } else {
+    console.log('価格が取得できませんでした');
+  }
 
   await browser.close();
 }
